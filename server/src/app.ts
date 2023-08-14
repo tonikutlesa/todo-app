33c0dbf102927ca07f6580d9616716856e904d44
Toni Kutlesa <toni.kutlesa@agilathon.com>
2023-08-14 17:29:53 +0200

    Add startServer function
    Add request logging, API rules, healthcheck route, handle unknown routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,15 +7,57 @@ const app = express();
 
 connectToDatabase()
   .then(() => {
-    app.listen(config.server.port, () => {
-      Logger.info(`Server listening on port ${config.server.port}`);
-    });
+    startServer();
   })
   .catch((error) => {
     Logger.error('Error starting the server:');
     Logger.error(error);
   });
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('test');
-});
+const startServer = () => {
+  app.use((req, res, next) => {
+    // Log the request
+    Logger.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+      // Log the response
+      Logger.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+    });
+
+    next();
+  });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // API rules
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  // Router
+
+  // Healthcheck route
+  app.get('/ping', (req, res, next) => res.status(200).json({ status: 'operating' }));
+
+  // Handle unknown route
+  app.use((req, res, next) => {
+    const error = new Error('Route not found');
+
+    Logger.error(error);
+
+    res.status(404).json({
+      message: error.message
+    });
+  });
+
+  app.listen(config.server.port, () => Logger.info(`Server is running on port ${config.server.port}`));
+};
