56af8e375298f1ed9e650c0c04aa9b7f327ae294
Toni Kutlesa <toni.kutlesa@agilathon.com>
2023-08-14 20:42:36 +0200

    Add Joi validation for create and update routes

---
+++ server/src/middleware/Joi.ts
@@ -0,0 +1,29 @@
+import Joi, { ObjectSchema } from 'joi';
+import { NextFunction, Request, Response } from 'express';
+import { ITodo } from '../types/todo';
+import Logger from '../utils/Logger';
+
+export const ValidateJoi = (schema: ObjectSchema) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validateAsync(req.body);
+
+      next();
+    } catch (error) {
+      Logger.error(error);
+      return res.status(422).json({ error });
+    }
+  };
+};
+
+export const Schemas = {
+  todo: {
+    create: Joi.object<ITodo>({
+      text: Joi.string().required()
+    }),
+    update: Joi.object<ITodo>({
+      text: Joi.string().required(),
+      done: Joi.boolean().optional()
+    })
+  }
+};
--- server/src/routes/todoRouter.ts
+++ server/src/routes/todoRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getAllTodos, getTodo, addTodo, updateTodo } from '../controllers/todo';
+import { Schemas, ValidateJoi } from '../middleware/Joi';
 
 const router: Router = Router();
 
 router.get('/', getAllTodos);
 router.get('/:id', getTodo);
-router.post('/', addTodo);
-router.put('/:id', updateTodo);
+router.post('/', ValidateJoi(Schemas.todo.create), addTodo);
+router.put('/:id', ValidateJoi(Schemas.todo.update), updateTodo);
 
 export default router;
